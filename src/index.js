51bed88371c7e66432e24f12fb8e9210139d391c
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2025-01-24 16:57:14 +0200

    startProg

--- src/index.js
+++ src/index.js
@@ -8,14 +8,19 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from 'redux/store';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="psychologists_services">
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render( <
+    React.StrictMode >
+    <
+    BrowserRouter basename = "virtualreality" >
+    <
+    Provider store = { store } >
+    <
+    PersistGate persistor = { persistor } >
+    <
+    App / >
+    <
+    /PersistGate> <
+    /Provider> <
+    /BrowserRouter> <
+    /React.StrictMode>
 );
